99c4bd5cee6f74422618ba855f9f30e494160f51
Arijit Basu <sayanarijit@gmail.com>
2021-06-24 08:16:44 +0530

    Init

---
+++ init.lua
@@ -0,0 +1,114 @@
+local function setup()
+  local xplr = xplr
+
+  xplr.config.general.default_ui.prefix = " "
+  xplr.config.general.default_ui.suffix = ""
+
+  xplr.config.general.focus_ui.prefix = "▸"
+  xplr.config.general.focus_ui.suffix = ""
+  xplr.config.general.focus_ui.style.fg = { Rgb = { 170, 150, 130} }
+  xplr.config.general.focus_ui.style.bg = { Rgb = { 50, 50, 50} }
+  xplr.config.general.focus_ui.style.add_modifiers = { "Bold" }
+
+  xplr.config.general.selection_ui.prefix = " "
+  xplr.config.general.selection_ui.suffix = ""
+  xplr.config.general.selection_ui.style.fg = { Rgb = { 70, 70, 70} }
+  xplr.config.general.selection_ui.style.add_modifiers = { "Bold", "CrossedOut" }
+
+  xplr.config.general.sort_and_filter_ui.separator.format = " » "
+  xplr.config.general.sort_and_filter_ui.separator.style.add_modifiers = { "Bold", "Reversed" }
+
+  xplr.config.general.panel_ui.default.title.style.bg = { Rgb = {170, 150, 130} }
+  xplr.config.general.panel_ui.default.title.style.fg = { Rgb = {40, 40, 40} }
+  xplr.config.general.panel_ui.default.title.style.add_modifiers = { "Bold" }
+  xplr.config.general.panel_ui.default.style.fg = { Rgb = { 170, 150, 130 } }
+  xplr.config.general.panel_ui.default.style.bg = { Rgb = { 33, 33, 33 } }
+  xplr.config.general.panel_ui.default.borders = {}
+  xplr.config.general.panel_ui.table.style.bg = { Rgb = { 26, 26, 26 } }
+
+  xplr.config.layouts.builtin.default.Vertical.config.constraints = {
+    { Length = 2 },
+    { Min = 1 },
+    { Length = 2 },
+  }
+  xplr.config.layouts.builtin.default.Vertical.splits = {
+    "SortAndFilter",
+    {
+      Horizontal = {
+        config = {
+          constraints = {
+            { Percentage = 20 },
+            { Percentage = 60 },
+            { Percentage = 20 }
+          }
+        },
+        splits={
+          "Selection",
+          "Table",
+          "HelpMenu",
+        }
+      }
+    },
+    "InputAndLogs",
+  }
+
+  xplr.config.layouts.builtin.no_help.Vertical.config.constraints = {
+    { Length = 2 },
+    { Min = 1 },
+  }
+  xplr.config.layouts.builtin.no_help.Vertical.splits = {
+    "SortAndFilter",
+    {
+      Horizontal = {
+        config = {
+          constraints = {
+            { Percentage = 80 },
+            { Percentage = 20 },
+          }
+        },
+        splits={
+          "Table",
+          "Selection"
+        }
+      }
+    },
+    "InputAndLogs",
+  }
+
+  xplr.config.layouts.builtin.no_selection.Vertical.config.constraints = {
+    { Length = 2 },
+    { Min = 1 },
+    { Length = 2 }
+  }
+  xplr.config.layouts.builtin.no_selection.Vertical.splits = {
+    "SortAndFilter",
+    {
+      Horizontal = {
+        config = {
+          constraints = {
+            { Percentage = 80 },
+            { Percentage = 20 }
+          }
+        },
+        splits = {
+          "Table",
+          "HelpMenu"
+        }
+      }
+    },
+    "InputAndLogs",
+  }
+
+  xplr.config.layouts.builtin.no_help_no_selection.Vertical.config.constraints = {
+    { Length = 2 },
+    { Min = 1 },
+    { Length = 2 }
+  }
+  xplr.config.layouts.builtin.no_help_no_selection.Vertical.splits = {
+    "SortAndFilter",
+    "Table",
+    "InputAndLogs"
+  }
+end
+
+return { setup = setup }
